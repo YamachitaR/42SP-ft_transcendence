da186aac216d526c8afabbb367c2c3f81e80a7f0
YamachitaR <rickyamas@gmail.com>
2024-06-16 17:23:39 -0300

    init Ricardo

--- RICARDO/srcs/django/ft_transcendence/settings.py
+++ RICARDO/srcs/django/ft_transcendence/settings.py
@@ -129,7 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+STATIC_URL = '/frontend/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
