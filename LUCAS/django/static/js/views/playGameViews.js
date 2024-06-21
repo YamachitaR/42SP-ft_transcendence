4bcd84620310d041c892084807f208554345f185
D4krSantana <emersonss13@gmail.com>
2024-06-21 16:37:32 -0300

    Inicio da Modificação

--- LUCAS/django/static/js/views/playGameViews.js
+++ LUCAS/django/static/js/views/playGameViews.js
@@ -1,18 +1,48 @@
 export function renderPlayGame() {
     return `
-        <div class="d-flex justify-content-center align-items-center min-vh-100">
-            <div class="card p-4" style="width: 80%; max-width: 800px; background-color: #333; border-color: #daa520;">
-                <div class="card-body">
-                    <h3 class="card-title text-center" style="color: #daa520;">Choose an Game Play</h3>
-                    <div class="d-grid gap-2">
-                        <button type="button" id="gameClassic" class="btn" style="background-color: #daa520; color: #333;">Game Clássico</button>
-                        <button type="button" id="game3d" class="btn btn-outline" style="border: 2px solid #daa520; color: #daa520;">Game 3D</button>
-                        <button type="button" id="game4players" class="btn" style="background-color: #000; color: #fff;">Game 4 Players</button>
-                        <button type="button" id="gameTorneio" class="btn" style="background-color: #007bff; color: #fff;">Game Torneio</button>
-                    </div>
-                    <div id="result" class="mt-4" style="color: #fff;"></div>
+        <!--Menu de Navegação do Site-->
+        <div class="conteiner">
+		    <div class="row h-2vh bg-body-color"></div>
+				<div id="nav-buttons" class="row h-6vh d-flex align-items-center justify-content-center my-bg-gold">
+					<div class="col-3"></div>
+					<div class="col-1"><button type="button" class="btn-custom-menu shadow w-100" data-route="/dashboard/">Dashboard</button></div>
+                    <div class="col-1"><button type="button" class="btn-custom-menu shadow w-100" data-route="/friends/">Friends</button></div>
+					<div class="col-1"><button type="button" class="btn-custom-menu shadow w-100" data-route="/playGame/">Game</button></div>
+					<div class="col-1"><button type="button" class="btn-custom-menu shadow w-100" data-route="/profile/">Profile</button></div>
+					<div class="col-1"><button type="button" class="btn-custom-menu shadow w-100" data-route="/logout/">Logout</button></div>
+					<div class="col-1 d-flex align-items-center justify-content-center">
+						<button type="button" class="btn-custom-menu shadow d-flex align-items-center justify-content-center"><img src="../img/country_flags/br.png" class="country-flags-size"></button>
+						<button type="button" class="btn-custom-menu shadow d-flex align-items-center justify-content-center"><img src="../img/country_flags/esp.png" class="country-flags-size"></button>
+						<button type="button" class="btn-custom-menu shadow d-flex align-items-center justify-content-center"><img src="../img/country_flags/eua.png" class="country-flags-size"></button>
+					</div>
+                    <div class="col-3"></div>
+				</div>
+			</div>
+        </div>
+
+        <!--Pagina Selecionar o modo de Jogo-->
+        <div class="conteiner bg-dashboard shadow m-4 rounded">
+            <div class="row d-flex align-items-center justify-content-center">
+                <div class="col-6 m-4  bg-dashboard-2 shadow text-center rounded">
+					<br><h4>Selecione o modo desejado</h4><br>
+				</div>
+            </div>
+			<br>
+            <div class="row">
+                <div class="col-3 d-flex align-items-center justify-content-center">
+                    <button type="button" id="gameClassic" class="btn-custom h-20vh w-100 m-4 shadow rounded">Classico 1x1</button>
+                </div>
+                <div class="col-3 d-flex align-items-center justify-content-center">
+                    <button type="button" id="game3d" class="btn-custom h-20vh w-100 m-4 shadow rounded">Jogo 3D</button>
+                </div>
+                <div class="col-3 d-flex align-items-center justify-content-center">
+                    <button type="button" id="game4players" class="btn-custom h-20vh w-100 m-4 shadow rounded">Modo 4 Jogadores</button>
+                </div>
+                <div class="col-3 d-flex align-items-center justify-content-center">
+                    <button type="button" id="gameTorneio" class="btn-custom h-20vh w-100 m-4 shadow rounded">Modo Torneio</button>
                 </div>
             </div>
+			<br><br>
         </div>
     `;
 }
--- LUCAS/django/static/js/views/userProfileViews.js
+++ LUCAS/django/static/js/views/userProfileViews.js
@@ -1,15 +1,25 @@
 export default function renderProfileUser() {
     return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{% static 'css/userprofile.css' %}">
-    <title>User Profile</title>
-</head>
-<body>
+        <!--Menu de Navegação do Site-->
+        <div class="conteiner">
+		    <div class="row h-2vh bg-body-color"></div>
+				<div id="nav-buttons" class="row h-6vh d-flex align-items-center justify-content-center my-bg-gold">
+					<div class="col-3"></div>
+					<div class="col-1"><button type="button" class="btn-custom-menu shadow w-100" data-route="/dashboard/">Dashboard</button></div>
+                    <div class="col-1"><button type="button" class="btn-custom-menu shadow w-100" data-route="/friends/">Friends</button></div>
+					<div class="col-1"><button type="button" class="btn-custom-menu shadow w-100" data-route="/playGame/">Game</button></div>
+					<div class="col-1"><button type="button" class="btn-custom-menu shadow w-100" data-route="/profile/">Profile</button></div>
+					<div class="col-1"><button type="button" class="btn-custom-menu shadow w-100" data-route="/logout/">Logout</button></div>
+					<div class="col-1 d-flex align-items-center justify-content-center">
+						<button type="button" class="btn-custom-menu shadow d-flex align-items-center justify-content-center"><img src="../img/country_flags/br.png" class="country-flags-size"></button>
+						<button type="button" class="btn-custom-menu shadow d-flex align-items-center justify-content-center"><img src="../img/country_flags/esp.png" class="country-flags-size"></button>
+						<button type="button" class="btn-custom-menu shadow d-flex align-items-center justify-content-center"><img src="../img/country_flags/eua.png" class="country-flags-size"></button>
+					</div>
+                    <div class="col-3"></div>
+				</div>
+			</div>
+        </div>
+
     <div class="container profile-container">
         <div class="row">
             <div class="col-md-4 profile-sidebar">
