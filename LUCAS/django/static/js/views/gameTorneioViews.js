2b62129015f0b943435b903439e29c4428dd359c
santzu <santzu@aioka.artes>
2024-06-25 22:24:06 -0300

    add victory

--- LUCAS/django/static/js/views/gameTorneioViews.js
+++ LUCAS/django/static/js/views/gameTorneioViews.js
@@ -143,3 +143,25 @@ export function gameTorneioPlayViews() {
   
     `;
 }
+
+export function gameTorneioVictoryViews() {
+    return `
+        <div class="conteiner bg-dashboard shadow m-4 rounded">
+
+            <div class="row d-flex align-items-center justify-content-center">
+                <div class="col-6 m-4 bg-dashboard-2 shadow text-center text-light rounded">
+                    <br><h4>Vitoria de: Jogador X</h4><br>
+                </div>
+            </div>
+
+            <div class="row d-flex align-items-center justify-content-center">
+                <div class="col-xs-10 col-sm-5 col-md-3 col-lg-3 col-xl-3 d-flex align-items-center justify-content-center">
+                    <button type="button" id="gameClassic" class="btn-custom p-4 shadow rounded">
+                        <h4>Voltar a Seleção</h4>
+                    </button>
+                </div>
+            </div>
+            <br><br>
+        </div>
+    `;
+}
