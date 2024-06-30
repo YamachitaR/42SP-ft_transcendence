7c7b2926c731038ba82d4f4613cb26f8d2641a14
D4rkSantana <emersonss13@gmail.com>
2024-06-29 22:16:25 -0300

    add pong IA

---
+++ LUCAS/django/static/js/pongIA/ball.js
@@ -0,0 +1,117 @@
+(function () {
+  'use strict';
+
+  if (typeof window.PongGameIA === "undefined") {
+    window.PongGameIA = {};
+  }
+
+
+  var Ball = window.PongGameIA.Ball = function(context, ball_color, ball_url, init_v) {
+    this.context = context;
+    this.ball_color = ball_color;
+    if (ball_url !== 'none'){
+      this.ball_image = new Image()
+      this.ball_image.src = ball_url;
+    }
+    else
+    {
+      this.ball_image = null
+    }
+    this.ball_url = ball_url;
+    this.position = [400, 250];
+    this.radius = 10;
+    this.speed = [init_v, init_v];
+    this.init_v = init_v;
+    this.hits = 0;
+    this.newHit = 0;
+  };
+
+  Ball.prototype.isTop = function() {
+    return (this.position[1] - this.radius) <= 0;
+  }
+
+  Ball.prototype.isBottom = function() {
+    return (this.position[1] + this.radius) > this.context.canvas.height;
+  } 
+
+  Ball.prototype.isLeft = function() {
+    return (this.position[0] + this.radius) < 0 && this.speed[0] < 0;
+  }
+
+  Ball.prototype.isRight = function() {
+    return (this.position[0] + this.radius) > this.context.canvas.width && this.speed[0] > 0;
+  }
+
+  Ball.prototype.moreLeft = function(x1, x2) {
+    return (this.position[0] - this.radius >= x1 && this.position[0] - this.radius <= x2);
+  }
+
+  Ball.prototype.moreRight = function(x1, x2) {
+    return (this.position[0] + this.radius >= x1 && this.position[0] + this.radius <=x2);
+  }
+
+  Ball.prototype.betweenY = function(y1, y2) {
+    return (this.position[1] >= y1 && this.position[1] <=y2);
+  }
+
+  Ball.prototype.move = function () {
+    if (this.isTop() || this.isBottom()) {
+      this.speed[1] = -this.speed[1];
+    } 
+    this.position[0] += this.speed[0];
+    this.position[1] += this.speed[1];
+  };
+
+  Ball.prototype.changeBallDirection = function() {
+    this.speed[0] = -this.speed[0];
+  }
+
+  Ball.prototype.resetIncrement = function () {
+    this.speed[0] = this.init_v;
+    this.speed[1] = this.init_v;
+    this.hits = 0;
+    this.newHit = 0;
+  };
+
+  Ball.prototype.checkHits = function () {
+    return (this.newHit > 0)
+  };
+
+  Ball.prototype.increaseBallSpeed = function () {
+    if (this.checkHits()) {
+      this.speed[0] += (this.speed[0] < 0) ? -0.5 : 0.5;
+      this.speed[1] += (this.speed[1] < 0) ? -0.5 : 0.5;
+      this.newHit = 0;
+    }
+
+  };
+
+  Ball.prototype.render = function () {
+    if ((this.ball_url !== 'none') && this.ball_image.complete) {
+      this.context.drawImage(this.ball_image, this.position[0] - this.radius, this.position[1] - this.radius, this.radius * 2, this.radius * 2);
+    }
+    else {
+      this.context.beginPath();
+      this.context.arc(this.position[0], this.position[1], this.radius, 0, 2 * Math.PI);
+      this.context.fillStyle = this.ball_color;
+      this.context.fill();
+    }
+  }
+
+  Ball.prototype.cleanup = function() {
+    // Removendo referências a objetos
+    this.context = null;
+    this.ball_color = null;
+    if (this.ball_image) {
+      this.ball_image.src = '';
+      this.ball_image = null;
+    }
+    this.ball_url = null;
+    this.position = null;
+    this.speed = null;
+    this.init_v = null;
+    this.hits = null;
+    this.newHit = null;
+    console.log('Recursos de Ball limpos');
+  };
+})();
---
+++ LUCAS/django/static/js/pongIA/pong.js
@@ -0,0 +1,246 @@
+(function () {
+  'use strict';
+
+  if (typeof window.PongGameIA === "undefined") {
+    window.PongGameIA = {};
+  }
+    var Game = PongGameIA.Game = function (canvas, defines) {
+      this.canvas = canvas;
+      this.context = canvas.getContext('2d');
+      this.canvas.width = defines.width;
+      this.canvas.height = defines.height;
+      this.ball = new PongGameIA.Ball(this.context, defines.ball_color, defines.ball_url, defines.init_v);
+      this.groud = new PongGameIA.Ground(this.context, defines.width, defines.height, defines.ground_color, defines.ground_url);
+
+      this.playerLeft = new PongGameIA.Player(this.context, "left", defines.l_color, defines.paddle_v);
+      this.playerRight = new PongGameIA.Player(this.context, "right", defines.r_color, defines.paddle_v);
+  
+      this.leftDetector = new PongGameIA.CollisionDetector(this.playerLeft, this.ball, this.context);
+      this.rightDetector = new PongGameIA.CollisionDetector(this.playerRight, this.ball, this.context);
+    
+      this.ia = new PongGameIA.Ia(this.playerRight, this.ball);
+
+      this.maxPoints = defines.MaxPoints;
+    
+      this.playerLeftName = defines.name_left;
+      this.playerRightName = defines.name_right;
+    
+      this.gameInterval = null;
+      this.jogoVivo = true;
+      this.jogoRodando = true;
+      this.winner = null;
+    
+      this.handleRouteChangeBound = this.handleRouteChange.bind(this);
+      this.locationChangeHandler = () => { window.dispatchEvent(new Event('locationchange')) };
+
+      document.addEventListener('visibilitychange', this.handleVisibilityChangeBound);
+      window.addEventListener('popstate', this.handleRouteChangeBound);
+
+      window.history.pushState = (f => {
+          const pushState = function pushState() {
+              const ret = f.apply(this, arguments);
+              window.dispatchEvent(new Event('pushstate'));
+              window.dispatchEvent(new Event('locationchange'));
+              return ret;
+          };
+          pushState._original = f;
+          return pushState;
+      })(window.history.pushState);
+      window.history.replaceState = (f => {
+          const replaceState = function replaceState() {
+              const ret = f.apply(this, arguments);
+              window.dispatchEvent(new Event('replacestate'));
+              window.dispatchEvent(new Event('locationchange'));
+              return ret;
+          };
+          replaceState._original = f;
+          return replaceState;
+      })(window.history.replaceState);
+      window.addEventListener('popstate', this.locationChangeHandler);
+      window.addEventListener('locationchange', this.handleRouteChangeBound);
+
+      console.log('Eventos visibilitychange e locationchange configurados');
+    };
+
+    Game.prototype.handleVisibilityChange = function () {
+      console.log('Evento visibilitychange disparado');
+      if (document.hidden) {
+        console.log('Saiu da pag do jogo');
+        clearInterval(this.gameInterval);
+        this.gameInterval = null;
+        this.jogoRodando = false;
+        clearInterval(this.gameInterval);
+      } else {
+        console.log('Retornou para a pag do jogo');
+        if (!this.gameInterval && (this.jogoVivo === true)) {
+          this.jogoRodando = true;
+          this.play();
+        }
+      }
+    };
+
+    Game.prototype.handleRouteChange = function () {
+      console.log('Evento de mudança de rota disparado');
+      if (window.location.pathname !== '/caminho-do-jogo') {
+        console.log('Saiu da página do jogo - Roteamento SPA');
+          clearInterval(this.gameInterval);
+          this.gameInterval = null;
+          this.jogoVivo = false;
+      }
+    };
+    
+    Game.prototype.renderBackground = function () {
+      if ((this.jogoVivo !== true) || !this.canvas || (this.jogoRodando !== true)) return;
+      this.context.fillStyle = 'white';
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.context.fillRect(this.canvas.width/2, 0, 2, this.canvas.height);
+    };
+
+    Game.prototype.render = function () {
+      if ((this.jogoVivo !== true) || (this.jogoRodando !== true)) return;
+      this.renderBackground();
+      this.groud.render();
+      if (this.ball) {
+        this.ball.move();
+        this.ball.render();
+        this.ball.increaseBallSpeed();
+      }
+      if (this.playerLeft && this.playerRight) {
+        this.playerLeft.render();
+        this.playerRight.render();
+        this.playerLeft.checkPaddlePosition();
+        this.playerRight.checkPaddlePosition();
+      }
+      if (this.leftDetector && this.rightDetector) {
+        this.leftDetector.checkCollision();
+        this.rightDetector.checkCollision();
+        this.leftDetector.score();
+        this.rightDetector.score();
+      }
+
+      if ((this.jogoVivo !== true) || (this.jogoRodando !== true)) return;
+      this.renderScores();
+      if (this.ia) {
+        this.ia.updateAI();
+      }
+
+      if (this.playerLeft.points >= this.maxPoints) {
+        this.jogoVivo = false;
+        this.showWinnerMessage(this.playerLeftName);
+        this.winner = this.playerLeftName;
+        clearInterval(this.gameInterval);
+        return this.winner;
+      } else if (this.playerRight.points >= this.maxPoints) {
+        this.jogoVivo = false;
+        this.showWinnerMessage(this.playerRightName);
+        this.winner = this.playerRightName;
+        clearInterval(this.gameInterval);
+        return this.winner;
+      }
+    };
+
+    Game.prototype.renderScores = function () {
+      if ((this.jogoVivo !== true) || (this.jogoRodando !== true)) return;
+      this.context.fillStyle = 'white';
+      this.context.font = '30px Tahoma';
+      this.context.fillText(this.playerLeft.points, this.canvas.width/4 *2 - 40, 40);
+      this.context.fillText(this.playerRight.points, this.canvas.width/4 *2 + 25, 40);
+    };
+  
+    Game.prototype.showWinnerMessage = function (winner) {
+      alert(winner + ' venceu o jogo com ' + this.maxPoints + ' pontos');
+    };
+
+    Game.prototype.play = function() {
+      if ((this.jogoVivo !== true) || (this.jogoRodando !== true)) return;
+      console.log('Game play started');
+      if (!this.gameInterval) {
+          this.gameInterval = setInterval(this.render.bind(this), 7);
+      }
+    };
+
+    Game.prototype.placar = function () {
+      if (this.jogoVivo !== true) {
+        return[this.playerLeft.points, this.playerRight.points];
+      }
+      else {
+        return null;
+      }
+    };
+
+    Game.prototype.gameFinish = function() {
+      const finished = !this.jogoVivo;
+      return finished;
+    };
+  
+    Game.prototype.getWinner = function() {
+      if (this.jogoVivo !== true) {
+          console.log('Winner is determined:', this.winner);
+          return this.winner;
+      } else {
+          console.log('No winner yet');
+          return 'none';
+      }
+    };
+  
+
+    Game.prototype.cleanup = function() {
+
+      document.removeEventListener('visibilitychange', this.handleVisibilityChangeBound);
+      window.removeEventListener('popstate', this.handleRouteChangeBound);
+
+      if (window.history.pushState._original) {
+          window.history.pushState = window.history.pushState._original;
+          delete window.history.pushState._original;
+      }
+      if (window.history.replaceState._original) {
+          window.history.replaceState = window.history.replaceState._original;
+          delete window.history.replaceState._original;
+      }
+
+      window.removeEventListener('popstate', this.locationChangeHandler);
+      window.removeEventListener('locationchange', this.handleRouteChangeBound);
+
+      if (this.gameInterval) {
+        clearInterval(this.gameInterval);
+        this.gameInterval = null;
+      }
+    
+      if (this.ball) {
+        this.ball.cleanup();
+        this.ball = null;
+      }
+      if (this.ground) {
+        this.ground.cleanup();
+        this.ground = null;
+      }
+      if (this.playerLeft) {
+        this.playerLeft.cleanup();
+        this.playerLeft = null;
+      }
+      if (this.playerRight) {
+        this.playerRight.cleanup();
+        this.playerRight = null;
+      }
+      if (this.leftDetector) {
+        this.leftDetector.cleanup();
+        this.leftDetector = null;
+      }
+      if (this.rightDetector) {
+        this.rightDetector.cleanup();
+        this.rightDetector = null;
+      }
+
+      this.canvas = null;
+      this.context = null;
+      this.maxPoints = null;
+      this.playerLeftName = null;
+      this.playerRightName = null;
+      this.jogoVivo = false;
+      this.jogoRodando = false;
+      this.winner = null;
+
+      console.log('Jogo limpo e recursos liberados');
+    };
+
+  })();
